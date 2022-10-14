bd4f2c060ce49cf1c12491ad03f3a2b7d70bdb75
Maksim Odnoletkov <odnoletkov@mail.ru>
2022-10-14 23:11:53 +0100

    Basic HTTP Kit webserver

--- src/server/core.clj
+++ src/server/core.clj
@@ -1,1 +1,12 @@
-(ns server.core)
+(ns server.core
+  (:require org.httpkit.server
+            compojure.core))
+
+(compojure.core/defroutes routes
+  (compojure.core/GET "/" []
+                      {:status 200}))
+
+(defn -main [& args]
+  (let [port (Integer/parseInt (System/getenv "PORT"))]
+    (org.httpkit.server/run-server #'routes {:port port})
+    (println (str "Running at http://localhost:" port))))
