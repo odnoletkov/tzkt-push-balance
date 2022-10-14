4f984e8dc062db57e2e8ff60d23d50561b9be0ea
Maksim Odnoletkov <odnoletkov@mail.ru>
2022-10-15 00:51:34 +0100

    Poll new txns

--- src/server/core.clj
+++ src/server/core.clj
@@ -1,12 +1,34 @@
 (ns server.core
   (:require org.httpkit.server
-            compojure.core))
+            org.httpkit.client
+            compojure.core
+            clojure.core.async))
 
 (compojure.core/defroutes routes
   (compojure.core/GET "/" []
                       {:status 200}))
 
+(def state (atom {}))
+
+(defn poll []
+  (let [level (:level @state)
+        level-query (if (nil? level) "level.lt=1" (str "level.gt=" level))
+        response (-> (str (System/getenv "TZKT_API") "/operations/transactions?" level-query)
+                     org.httpkit.client/get
+                     deref)
+        ; TODO: handle limit
+        {{new-level :tzkt-level} :headers} response]
+    (swap! state assoc :level new-level)))
+
 (defn -main [& args]
   (let [port (Integer/parseInt (System/getenv "PORT"))]
+
+    (clojure.core.async/go-loop []
+                                (do
+                                  ; TODO: error handling
+                                  (poll)
+                                  (Thread/sleep (Integer/parseInt (System/getenv "POLL_MS")))
+                                  (recur)))
+
     (org.httpkit.server/run-server #'routes {:port port})
     (println (str "Running at http://localhost:" port))))
