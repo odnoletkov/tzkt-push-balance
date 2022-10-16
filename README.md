a528adaee28f78ac5b06c8198747bde3ce91ac0d
Maksim Odnoletkov <odnoletkov@mail.ru>
2022-10-16 03:41:11 +0100

    Cleanup

--- README.md
+++ README.md
@@ -1,12 +1,46 @@
 # TZKT Push Balance Demo
 
-## Run
+WebSocket server that tracks Tezos blockchain progress and pushes balance updates to connected clients.
+
+* Client POV:
+  * Connects via WebSocket to track specific Tezos address
+  * Receives current balance on initial connection
+  * Receives 'real time' balance updates
+  * Exactly one update per each relevant transaction
+  * Balance updates are strictly ordered
+
+* Server POV:
+  * Fetches balance for specific Tezos address ~once (barring racing clients and level coordination between APIs)
+  * Tracks blockchain progress via poll
+  * Fails explicitly on network errors
+  * Uses [TZKT REST APIs](https://api.tzkt.io):
+    * [/operations/transactions](https://api.tzkt.io/#operation/Operations_GetTransactions)
+    * [/accounts/{address}](https://api.tzkt.io/#operation/Accounts_GetByAddress)
+  * Assumes response body is always consistent with `tzkt-level` header
+  * Relies on eventual `tzkt-level` coordination between APIs for progress (not for correctness)
+
+## Install
 
     brew install clojure
 
-    PORT=3000 TZKT_API=https://api.tzkt.io/v1 POLL_MS=1000 clj -M -m server.core
+## Run Server
+
+    PORT=3000 clj -M -m server.core
+
+## Test with curl
 
     curl --no-buffer \
-        --header "Upgrade: websocket" \
-        --header "Sec-WebSocket-Key: ." \
-        localhost:3000/ws/tz1dtzgLYUHMhP6sWeFtFsHkHqyPezBBPLsZ
+            --header "Upgrade: websocket" \
+            --header "Sec-WebSocket-Key: ." \
+            localhost:3000/ws/tz1dtzgLYUHMhP6sWeFtFsHkHqyPezBBPLsZ
+
+Sample output:
+
+```
+?
+5405017337?
+5405067337?
+5405073562?
+5405081062?
+5405106062
+```
--- src/server/core.clj
+++ src/server/core.clj
@@ -4,36 +4,65 @@
             compojure.core
             [clojure.data.json :as json]))
 
-(def state (agent {:level nil
-                   :addresses {"some-address" {:clients #{}
-                                               :balance nil}}}))
+(def state
+  "State of the service"
+  (agent {:level nil
+          :addresses {"some-address" {:clients #{}
+                                      :balance nil}}}))
 
-(defn api [& parts]
-  (-> (apply str (or (System/getenv "TZKT_API") "https://api.tzkt.io/v1") parts)
+(defn api
+  "Call TZKT API synchronously"
+  [& url-parts]
+  (-> (or (System/getenv "TZKT_API") "https://api.tzkt.io/v1")
+      (str (apply str url-parts))
       org.httpkit.client/get
       deref
-      (doto (-> :status (= 200) assert))))
+      (doto (-> :status (= 200) assert))
+      (update :body json/read-str)))
+
+;;;; Manage WebSocket clients
+
+(defn add-client-when-know-balance! [address ch st]
+  ;; Send initial value.
+  (server/send! ch (-> st
+                       :addresses
+                       (get address)
+                       :balance
+                       (doto (-> some? assert))
+                       str))
+  ;; Add client.
+  (update-in st [:addresses address :clients] #(-> % set (conj ch))))
+
+(defn get-balance [address]
+  (let [{{level :tzkt-level} :headers
+         {:strs [balance lastActivity]} :body} (api "/accounts/" address)]
+    [balance [lastActivity (Integer/parseInt level)]]))
+
+(defn try-add-address [address
+                       [balance [last-activity level-of-balance]]
+                       {current-level :level :as st}]
+  ;; Is fetched balance snapshot consistent with local state?
+  ;;  * No if it includes activity after local level (don't double count)
+  ;;  * No if it is older than local level (don't miss events)
+  (if (<= last-activity current-level level-of-balance)
+    ;; Yes - add address to local state.
+    (assoc-in st [:addresses address :balance] balance)
+    ;; No - need to try again.
+    st))
 
 (defn add-client! [address ch]
+  ;; Address tracked already?
+  ;; Read state opportunistically - racing clients will cause redundant balance fetches
   (if (-> @state :addresses (get address) :balance some?)
-
-    (send-off
-      state
-      (fn [st]
-        (server/send! ch (-> st :addresses (get address) :balance str))
-        (update-in st [:addresses address :clients] #(-> % set (conj ch)))))
-
-    (let [account (api "/accounts/" address)
-          {{level-of-balance :tzkt-level} :headers} account
-          {:strs [balance lastActivity]} (-> account :body json/read-str)]
-
-      (await
-        (send state (fn [{current-level :level :as st}]
-                      (if (<= lastActivity current-level (Integer/parseInt level-of-balance))
-                        (assoc-in st [:addresses address :balance] balance)
-                        st))))
-
+    ;; Yes - just add new client.
+    (send-off state (partial add-client-when-know-balance! address ch))
+    ;; No - add new address.
+    (do
+      ;; Try to add new address.
+      (await (send state (partial try-add-address address (get-balance address))))
+      ;; Let local state progress.
       (Thread/sleep 1000)
+      ;; Try to add client again.
       (recur address ch))))
 
 (defn remove-client! [address ch _]
@@ -47,40 +76,64 @@
 (compojure.core/defroutes routes
   (compojure.core/GET "/ws/:address" [] websocket-handler))
 
+;;;; Poll for new transactions and update state
+
 (defn update-existing [m k f]
   (if-let [kv (find m k)] (assoc m k (f (val kv))) m))
 
-(defn notify-address! [{:keys [balance clients]}]
+(defn notify-address!
+  "Notify all clients of the given address"
+  [{:keys [balance clients]}]
   (doseq [ch clients]
     (server/send! ch (str balance))))
 
-(defn update-address! [delta st]
+(defn update-address!
+  "Update balance of the given address"
+  [delta st]
   (-> st
       (update :balance #(+ % delta))
       (doto notify-address!)))
 
-(defn handle-tx! [st {{sender :address} :sender
-                      {target :address} :target
-                      amount :amount}]
+(defn handle-tx!
+  "Update state with transaction"
+  [st {{sender "address"} "sender"
+       {target "address"} "target"
+       amount "amount"}]
   (update st :addresses
           #(-> %
                (update-existing sender (partial update-address! (- amount)))
                (update-existing target (partial update-address! (+ amount))))))
 
-(defn poll! []
-  (let [level (:level @state)
-        level-query (if (nil? level) "level.lt=1" (str "level.gt=" level))
-        limit 10000
-        response (api "/operations/transactions?amount.gt=0&select=sender,target,amount&limit=" limit "&" level-query)
-        txns (-> response :body (json/read-str :key-fn keyword))
-        {{new-level :tzkt-level} :headers} response]
-    (assert (< (count txns) limit))
+(defn get-new-transactions [since-level]
+  (let [limit 10000
+        ;; Query filter for new transactions.
+        level-query (if (nil? since-level)
+                      "level.lt=1" ; request empty txns for the initial state
+                      (str "level.gt=" since-level))
+        ;; Get new transactions.
+        {{new-level :tzkt-level} :headers
+         txns :body} (api "/operations/transactions"
+                          "?" level-query
+                          "&amount.gt=0"
+                          "&select=sender,target,amount"
+                          "&sort.asc=id"
+                          "&limit=" limit)]
+    (assert (< (count txns) limit) "fetch all new transactions")
+    [txns (Integer/parseInt new-level)]))
+
+(defn poll!
+  "Get new transactions and update state"
+  []
+  (let [[txns new-level] (get-new-transactions (:level @state))]
     (await (send-off state #(as-> % st
-                              (assoc st :level (Integer/parseInt new-level))
+                              ;; Update level.
+                              (assoc st :level new-level)
+                              ;; Update balances and notify clients.
                               (reduce handle-tx! st txns))))))
 
 (defn -main []
 
+  ;; Start WebSocket server.
   (->> (some->>
          (System/getenv "PORT")
          Integer/parseInt
@@ -88,11 +141,13 @@
        (merge {:legacy-return-value? false})
        (server/run-server #'routes)
        server/server-port
-       (str "Listening at http://localhost:")
+       (str "Listening on http://localhost:")
        println)
 
+  ;; Poll and handle new transactions in a loop.
   (future
     (while true
       (do
         (poll!)
+        ;; Sleep until next poll
         (Thread/sleep (-> (System/getenv "POLL_MS") (or "1000") Integer/parseInt))))))
